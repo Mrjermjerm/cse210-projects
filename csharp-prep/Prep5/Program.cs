b8f5d7846deee2c83b4c84374e2368394342ed81
Jeremy Sanders <jeremyedsanders@byui.edu>
2024-11-02 14:13:04 -0600

    Assignment 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Xml.XPath;
 
 class Program
 {
+    static void DisplayWelcome()
+        {
+            Console.WriteLine($"Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write($"Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write($"Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareResult(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int result = SquareResult(favoriteNumber);
+
+        Console.WriteLine($"{username}, the square of your number is {result}");
+        
     }
 }
