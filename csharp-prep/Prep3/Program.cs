4c0ed4d28032d438be9c1604180afdc85f0cb959
Jeremy Sanders <jeremyedsanders@byui.edu>
2024-11-02 11:30:37 -0600

    assignment 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        
+        do
+        {
+            // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            int guess = -1;
+            int numberOfGuesses = 0;
+
+            while (number != guess)
+            { 
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                ++numberOfGuesses;
+
+                if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else 
+                {
+                    Console.WriteLine("You guessed it");
+                    Console.WriteLine($"You gessed {numberOfGuesses} times.");
+                }
+                
+            }
+        
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
